8f646b5622d3dbca2aeef91730e18a05633fa094
Enzo CORSICO <58816644+enzocorsico@users.noreply.github.com>
2022-07-04 16:07:58 +0200

    Guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,6 +8,7 @@ import { AuthGuard } from '@nestjs/passport';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(AuthGuard("jwt"))
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
@@ -19,21 +20,25 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @UseGuards(AuthGuard("jwt"))
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findOne(id);
   }
 
+  @UseGuards(AuthGuard("jwt"))
   @Get('username/:username')
   findByUsername(@Param('username') username: string) {
     return this.userService.findByUsername(username);
   }
 
+  @UseGuards(AuthGuard("jwt"))
   @Put(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
 
+  @UseGuards(AuthGuard("jwt"))
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.userService.remove(id);
