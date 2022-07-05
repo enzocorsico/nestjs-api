b034bb73cc5e17a864baea55cebb10b131742e58
Enzo CORSICO <58816644+enzocorsico@users.noreply.github.com>
2022-07-05 11:53:33 +0200

    Chemin fichier d'environnement

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [".env.local", ".env"]
+      envFilePath: ["./.env.local", "./.env"]
     }),
     TypeOrmModule.forRoot({
       type: "mysql",
