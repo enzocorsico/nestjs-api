ab10ff0b07274fc94e28467bdeb8b05cd46747cc
Enzo CORSICO <58816644+enzocorsico@users.noreply.github.com>
2022-07-05 14:31:12 +0200

    Docker + Docker prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,11 +15,11 @@ import { AuthModule } from './auth/auth.module';
     }),
     TypeOrmModule.forRoot({
       type: "mysql",
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       entities: [User],
       synchronize: true
     }),
